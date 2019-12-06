8d719994b27ed1a44e8f6a71b83cb61dc854ee84
Andrey <lazarev.and_18@list.ru>
2019-12-06 14:59:32 +0300

    Form

---
+++ submit.php
@@ -0,0 +1,37 @@
+<?php
+ 
+/* Задаем переменные */
+$name = htmlspecialchars($_POST["name"]);
+$email = htmlspecialchars($_POST["email"]);
+$subj = htmlspecialchars($_POST["subject"]);
+$message = htmlspecialchars($_POST["message"]);
+ 
+/* Ваш адрес и тема сообщения */
+$address = "selo.moloko@gmail.com";
+$sub = "Сообщение с ForPig.ru";
+ 
+/* Формат письма */
+$mes = "Сообщение с ForPig.ru\n
+Имя отправителя: $name 
+Электронный адрес отправителя: $email
+Тема: $subj
+Текст сообщения: $massage";
+ 
+ 
+if (empty($bezspama)) /* Оценка поля bezspama - должно быть пустым*/
+{
+/* Отправляем сообщение, используя mail() функцию */
+$from  = "From: $name <$email> \r\n Reply-To: $email \r\n";
+if (mail($address, $sub, $mes, $from)) {
+	header('Refresh: 1; URL=http://icft.eu');
+	echo '<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+    <body>Письмо отправлено, через 5 секунд вы вернетесь на Forpig.ru</body>';}
+else {
+	header('Refresh: 3; URL=http://icft.eu');
+	echo '<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+    <body>Письмо НЕ отправлено, через 5 секунд вы вернетесь на </body>';}
+}
+exit; /* Выход без сообщения, если поле bezspama заполнено спам ботами */
+?>
