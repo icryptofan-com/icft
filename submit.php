3af525f01cff1f2d067f793a56f9e9ae608d21dc
Andrey <lazarev.and_18@list.ru>
2019-12-06 15:01:56 +0300

    Update submit.php

--- submit.php
+++ submit.php
@@ -8,10 +8,10 @@ $message = htmlspecialchars($_POST["message"]);
  
 /* Ваш адрес и тема сообщения */
 $address = "selo.moloko@gmail.com";
-$sub = "Сообщение с ForPig.ru";
+$sub = "Сообщение с ICFt.eu";
  
 /* Формат письма */
-$mes = "Сообщение с ForPig.ru\n
+$mes = "Сообщение с icft.eu\n
 Имя отправителя: $name 
 Электронный адрес отправителя: $email
 Тема: $subj
@@ -26,12 +26,12 @@ if (mail($address, $sub, $mes, $from)) {
 	header('Refresh: 1; URL=http://icft.eu');
 	echo '<head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
-    <body>Письмо отправлено, через 5 секунд вы вернетесь на Forpig.ru</body>';}
+    <body>Письмо отправлено, через 5 секунд вы вернетесь на icft.eu</body>';}
 else {
 	header('Refresh: 3; URL=http://icft.eu');
 	echo '<head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
-    <body>Письмо НЕ отправлено, через 5 секунд вы вернетесь на </body>';}
+    <body>Письмо НЕ отправлено, через 5 секунд вы вернетесь на icft.eu</body>';}
 }
 exit; /* Выход без сообщения, если поле bezspama заполнено спам ботами */
 ?>
